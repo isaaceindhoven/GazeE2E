3e301e987ad0738484d2d3376848b7f475a828e9
BaderAli-ISAAC <80455868+BaderAli-ISAAC@users.noreply.github.com>
2021-05-28 11:19:18 +0200

    Add tests for unauthenticate and reset methods in GazeClient (#2)
    Add tests for unauthenticate and reset methods

--- tests/gaze.test.js
+++ tests/gaze.test.js
@@ -1,149 +1,149 @@
-const { emit, pageTest } = require('./utils/helpers')
-const { startTests } = require('./runner')
+const { emit, pageTest } = require('./utils/helpers');
+const { startTests } = require('./runner');
 
 startTests(['chromium', 'firefox', 'webkit'], [
 
     pageTest('if client receives payload', async page => {
         await page.evaluate(async () => {
             await gaze.on(() => ['event1'], printToDom)
-        })
-        await emit('event1', {'id' : 1})
+        });
+        await emit('event1', {'id' : 1});
         await page.assertSee({'id' : 1})
     }),
 
     pageTest('if client updates subscription', async page => {
         await page.evaluate(async () => {
-            window.topics = ['event1']
-            window.sub1 = await gaze.on(() => window.topics, printToDom)
-            window.topics = ['event2']
+            window.topics = ['event1'];
+            window.sub1 = await gaze.on(() => window.topics, printToDom);
+            window.topics = ['event2'];
             await window.sub1.update()
-        })
+        });
 
-        await emit('event1', {'name' : 'kevin'})
-        await emit('event2', {'name' : 'patrik'})
+        await emit('event1', {'name' : 'kevin'});
+        await emit('event2', {'name' : 'patrik'});
 
-        await page.assertDontSee({'name' : 'kevin'})
+        await page.assertDontSee({'name' : 'kevin'});
         await page.assertSee({'name' : 'patrik'})
     }),
 
     pageTest('if client updates after on had empty array', async page => {
         await page.evaluate(async () => {
-            window.topics = []
-            window.sub1 = await gaze.on(() => window.topics, printToDom)
-            window.topics = ['event2']
+            window.topics = [];
+            window.sub1 = await gaze.on(() => window.topics, printToDom);
+            window.topics = ['event2'];
             await window.sub1.update()
-        })
+        });
 
-        await emit('event1', {'name' : 'kevin'})
-        await emit('event2', {'name' : 'patrik'})
+        await emit('event1', {'name' : 'kevin'});
+        await emit('event2', {'name' : 'patrik'});
 
-        await page.assertDontSee({'name' : 'kevin'})
+        await page.assertDontSee({'name' : 'kevin'});
         await page.assertSee({'name' : 'patrik'})
     }),
 
     pageTest('if client does not receive other events', async page => {
         await page.evaluate(async () => {
             await gaze.on(() => ['event2'], printToDom)
-        })
-        await emit('event1', {'name' : 'kevin'})
+        });
+        await emit('event1', {'name' : 'kevin'});
         await page.assertDontSee({'name' : 'kevin'})
     }),
 
     pageTest('if public event is received', async page => {
         await page.evaluate(async () => {
-            await init(['admin'])
+            await init(['admin']);
             await gaze.on(() => ['event1'], printToDom)
-        })
-        await emit('event1', {'name' : 'kevin'})
+        });
+        await emit('event1', {'name' : 'kevin'});
         await page.assertSee({'name' : 'kevin'})
     }),
 
     pageTest('if admin event is not received by guest', async page => {
         await page.evaluate(async () => {
             await gaze.on(() => ['event1'], printToDom)
-        })
-        await emit('event1', {'name' : 'kevin'}, 'admin')
+        });
+        await emit('event1', {'name' : 'kevin'}, 'admin');
         await page.assertDontSee({'name' : 'kevin'})
     }),
 
     pageTest('if admin event is received by admin role', async page => {
         await page.evaluate(async () => {
-            await init(['admin'])
+            await init(['admin']);
             await gaze.on(() => ['event1'], printToDom)
-        })
-        await emit('event1', {'name' : 'kevin'}, 'admin')
+        });
+        await emit('event1', {'name' : 'kevin'}, 'admin');
         await page.assertSee({'name' : 'kevin'})
     }),
 
     pageTest('if payload callback is called once', async page => {
         await page.evaluate(async () => {
             await gaze.on(() => ['event1', 'event2'], printToDom)
-        })
-        await emit('event1', {'name' : 'kevin'})
+        });
+        await emit('event1', {'name' : 'kevin'});
         await page.assertSee({'name' : 'kevin'})
     }),
 
     pageTest('if event name is case sensative', async page => {
         await page.evaluate(async () => {
             await gaze.on(() => ['Event1'], printToDom)
-        })
-        await emit('event1', {'name' : 'kevin'})
+        });
+        await emit('event1', {'name' : 'kevin'});
         await page.assertDontSee({'name' : 'kevin'})
     }),
 
     pageTest('if subscription is made with empty values', async page => {
         await page.evaluate(async () => {
             await gaze.on(() => ['', null, undefined], printToDom)
-        })
+        });
 
-        let subscriptions = await page.evaluate(() => gaze.subscriptions.getAll())
+        let subscriptions = await page.evaluate(() => gaze.subscriptions.getAll());
 
-        expect(subscriptions.length).toBe(1)
+        expect(subscriptions.length).toBe(1);
         expect(subscriptions[0].topics.length).toBe(0)
     }),
     
     pageTest('if subscription is not made if topics are not a list', async page => {
         await page.evaluate(async () => {
-            await gaze.on(() => false, printToDom)
-            await gaze.on(() => {}, printToDom)
+            await gaze.on(() => false, printToDom);
+            await gaze.on(() => {}, printToDom);
             await gaze.on(() => 100, printToDom)
-        })
+        });
 
-        let subscriptions = await page.evaluate(() => gaze.subscriptions.getAll())
+        let subscriptions = await page.evaluate(() => gaze.subscriptions.getAll());
 
-        expect(subscriptions.length).toBe(3)
-        expect(subscriptions[0].topics.length).toBe(0)
-        expect(subscriptions[1].topics.length).toBe(0)
+        expect(subscriptions.length).toBe(3);
+        expect(subscriptions[0].topics.length).toBe(0);
+        expect(subscriptions[1].topics.length).toBe(0);
         expect(subscriptions[2].topics.length).toBe(0)
     }),
 
     pageTest('if callback does not break javascript', async page => {
         await page.evaluate(async () => {
-            await gaze.on(() => ['BrokenEventSync'], () => { throw 'Error'})
+            await gaze.on(() => ['BrokenEventSync'], () => { throw 'Error'});
             await gaze.on(() => ['WorkingEvent'], printToDom)
-        })
+        });
 
-        await emit('BrokenEventSync', {'name' : 'kevin'})
-        await emit('WorkingEvent', {'name' : 'piet'})
+        await emit('BrokenEventSync', {'name' : 'kevin'});
+        await emit('WorkingEvent', {'name' : 'piet'});
 
-        await page.assertSee({'name' : 'piet'})
+        await page.assertSee({'name' : 'piet'});
         await page.assertDontSee({'name' : 'kevin'})
     }),
 
     pageTest('if on can accept different topics types', async page => {
         await page.evaluate(async () => {
-            await gaze.on(() => ['Type1'], printToDom)
-            await gaze.on(['Type2'], printToDom)
+            await gaze.on(() => ['Type1'], printToDom);
+            await gaze.on(['Type2'], printToDom);
             await gaze.on('Type3', printToDom)
-        })
+        });
 
-        await emit('Type1', {'name' : 'Type1'})
-        await emit('Type2', {'name' : 'Type2'})
-        await emit('Type3', {'name' : 'Type3'})
+        await emit('Type1', {'name' : 'Type1'});
+        await emit('Type2', {'name' : 'Type2'});
+        await emit('Type3', {'name' : 'Type3'});
 
-        await page.assertSee({'name' : 'Type1'})
-        await page.assertSee({'name' : 'Type2'})
-        await page.assertSee({'name' : 'Type3'})
+        await page.assertSee({'name' : 'Type1'});
+        await page.assertSee({'name' : 'Type2'});
+        await page.assertSee({'name' : 'Type3'});
     }),
 
     pageTest('if single middleware works with payload change', async page => {
@@ -155,9 +155,9 @@ startTests(['chromium', 'firefox', 'webkit'], [
             });
 
             await gaze.on('new_names', printToDom)
-        })
+        });
 
-        await emit('new_names', {'name' : 'Kevin'})
+        await emit('new_names', {'name' : 'Kevin'});
 
         await page.assertSee({'name' : 'Bob'})
     }),
@@ -181,9 +181,9 @@ startTests(['chromium', 'firefox', 'webkit'], [
             });
 
             await gaze.on('new_names', printToDom)
-        })
+        });
 
-        await emit('new_names', {'name' : 'Kevin'})
+        await emit('new_names', {'name' : 'Kevin'});
 
         await page.assertSee({'name' : 'Kevin is a cool dude'})
     }),
@@ -197,12 +197,12 @@ startTests(['chromium', 'firefox', 'webkit'], [
             });
 
             await gaze.on('new_names', printToDom)
-        })
+        });
 
-        await emit('new_names', {'name' : 'Kevin'})
-        await emit('new_names', {'name' : 'Bob'})
+        await emit('new_names', {'name' : 'Kevin'});
+        await emit('new_names', {'name' : 'Bob'});
 
-        await page.assertSee({'name' : 'Kevin'})
+        await page.assertSee({'name' : 'Kevin'});
         await page.assertDontSee({'name' : 'Bob'})
     }),
 
@@ -217,13 +217,38 @@ startTests(['chromium', 'firefox', 'webkit'], [
             });
 
             await gaze.on('new_names', printToDom)
-        })
+        });
 
-        await emit('new_names', {'name' : 'Bob'})
-        await emit('new_names', {'name' : 'Kevin'})
+        await emit('new_names', {'name' : 'Bob'});
+        await emit('new_names', {'name' : 'Kevin'});
 
-        await page.assertDontSee({'name' : 'Bob'})
+        await page.assertDontSee({'name' : 'Bob'});
         await page.assertSee({'name' : 'Kevin'})
     }),
 
-])
+    pageTest('if gaze unauthenticate does not receive authenticated events', async page => {
+        await page.evaluate(async () => {
+            await init(['admin']);
+            await gaze.on('Event1', printToDom);
+            await gaze.unauthenticate();
+        });
+
+        await emit('Event1', {'name' : 'Event1'}, 'admin');
+
+        await page.assertDontSee({'name' : 'Event1'});
+    }),
+
+    pageTest('if gaze reset removes all subscriptions', async page => {
+        await page.evaluate(async () => {
+            await gaze.on('Event1', printToDom);
+            await gaze.on('Event2', printToDom);
+            await gaze.reset();
+        });
+
+        await emit('Event1', {'name' : 'Event1'});
+        await emit('Event2', {'name' : 'Event2'});
+
+        await page.assertDontSee({'name' : 'Event1'});
+        await page.assertDontSee({'name' : 'Event2'});
+    }),
+]);
--- tests/runner.js
+++ tests/runner.js
@@ -1,8 +1,8 @@
-const { openClientPage } = require('./utils/helpers')
-const playwright = require('playwright')
-const { dockerUp, dockerKill } = require('./utils/DockerCompose')(process.cwd())
+const { openClientPage } = require('./utils/helpers');
+const playwright = require('playwright');
+const { dockerUp, dockerKill } = require('./utils/DockerCompose')(process.cwd());
 
-require('dotenv').config()
+require('dotenv').config();
 
 function startTests(browsers, tests){
 
@@ -10,40 +10,40 @@ function startTests(browsers, tests){
         browsers = browsers.filter(b => b.includes('only:')).map(b => b.replace('only:', ''))
     }
 
-    jest.setTimeout(30 * 1000)
+    jest.setTimeout(30 * 1000);
 
     beforeAll(async () => {
         await dockerUp('Server running on')
-    }, 45 * 1000)
+    }, 45 * 1000);
 
     afterAll(async () => {
         await dockerKill()
-    }, 45 * 1000)
+    }, 45 * 1000);
 
     for(let browserType of browsers){
         describe(`Testing with browser ${browserType}`, () => {
     
-            let browser = null
-            let page = null
+            let browser = null;
+            let page = null;
     
             beforeAll(async () => {
                 browser = await playwright[browserType].launch({'headless' : true})
-            }, 45 * 1000)
+            }, 45 * 1000);
     
             beforeEach(async () => {
-                page = await openClientPage(browser)
+                page = await openClientPage(browser);
                 await page.evaluate(async () => await init())
-            })
+            });
     
             afterEach(async () => {
                 for(let c of browser.contexts()){ 
                     await c.close() 
                 }
-            })
+            });
     
             afterAll(async () => { 
                 await browser.close() 
-            })
+            });
 
             if (tests.some(t => t.name.includes('only:'))){
                 tests = tests.filter(t => t.name.includes('only:'))
@@ -58,4 +58,4 @@ function startTests(browsers, tests){
 
 }
 
-module.exports = { startTests }
+module.exports = { startTests };
